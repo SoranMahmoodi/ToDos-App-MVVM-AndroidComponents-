f894fbac814534092be3e8f3e1aeef6bb67afc80
Soran Mah <soranmahmoodi.com@gmail.com>
2020-08-28 00:50:19 -0700

    init commit

--- app/src/main/java/com/example/myapplication/utiliti/CheckedIsCompleted.java
+++
@@ -1,9 +0,0 @@
-package com.example.myapplication.utiliti;
-
-import android.widget.CompoundButton;
-
-public interface CheckedIsCompleted {
-    void onUserChangeChecked(CompoundButton cb, boolean isChecked);
-
-}
-
--- app/src/main/java/com/example/myapplication/utils/Constants.java
+++ app/src/main/java/com/example/myapplication/utils/Constants.java
@@ -1,4 +1,4 @@
-package com.example.myapplication.utiliti;
+package com.example.myapplication.utils;
 
 public interface Constants {
     public static final int KEY_CODE = 1010;
