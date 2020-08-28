01207f581a242c747914b82cca759b3e4c8fc1d8
Soran Mah <soranmahmoodi.com@gmail.com>
2020-08-27 23:54:28 -0700

    init commit

---
+++ app/src/main/java/com/example/myapplication/utiliti/CheckedIsCompleted.java
@@ -0,0 +1,9 @@
+package com.example.myapplication.utiliti;
+
+import android.widget.CompoundButton;
+
+public interface CheckedIsCompleted {
+    void onUserChangeChecked(CompoundButton cb, boolean isChecked);
+
+}
+
